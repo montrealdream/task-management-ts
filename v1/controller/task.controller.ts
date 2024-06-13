41c216a82dc2708caf05f23db3d88f8b6b91e8a0
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-06-13 21:42:41 +0700

    bộ lọc trạng thái công việc

--- v1/controller/task.controller.ts
+++ v1/controller/task.controller.ts
@@ -3,17 +3,47 @@ import { Response , Request } from "express";
 
 // model
 import Task from "../models/task.model";
+import { stat } from "fs";
 
 export const index = async (req: Request, res: Response): Promise<void> => {
     try{
-        const tasks = await Task.find({
+        // Create Interface for object Find
+        interface objFind {
+            deleted: Boolean,
+            status?: String,
+        }
+
+        const objFind: objFind = {
             deleted: false
-        });
+        }
+
+        // filter status
+        const listStatus: string[] = ["initial", "doing", "pending", "finish", "notFinish"];
+
+        if(req.query.status){
+            const status = `${req.query.status}`;
+
+            if(listStatus.includes(`${status}`)){
+                
+                objFind["status"] = `${status}`; // lưu ý khi không có interface sẽ không gán thêm trường dữ liệu được
+            }
+            else {
+                // 400 Bad Request: Request không hợp lệ
+                res.status(400).json({
+                    code: 400,
+                    message: "Request status không hợp lệ"
+                });
+                return;
+            }
+        }
+        
+        const tasks = await Task.find(objFind).select('title content');
 
         // response json
         res.status(200).json({
             code: 200,
-            message: "Đã lấy ra danh sách công việc"
+            message: "Đã lấy ra danh sách công việc",
+            tasks
         });
     }
     catch(error){
